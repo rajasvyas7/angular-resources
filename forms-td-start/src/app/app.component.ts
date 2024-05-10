27c8e41f08a1068fd1ba4d925c6b467ddcc2fc0e
Rajas Vyas <rajas.vyas@gmail.com>
2024-05-10 18:49:00 +0530

    Using ngModel to get the form childs, using local reference with ngForm to get form object; validations based on angula classes; default values for inputs ; and 2 way binding

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("f") vcForm: NgForm;
+
+  //for using default value at the dropdown
+  defaultQuestion = "teacher";
+
+  //to show 2 way binding
+  answer = "Not answered yet."
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  /**
+   * one way is to pass the form object as paramter to the submit function
+   */
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  /**
+   * another way is to use @viewchild
+   */
+  onSubmit() {
+    console.log(this.vcForm);
   }
 }
