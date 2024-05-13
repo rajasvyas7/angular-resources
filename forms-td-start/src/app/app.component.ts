50e2df9d119bd74f571ce168c599251d254fc42d
Rajas Vyas <rajas.vyas@gmail.com>
2024-05-13 19:50:37 +0530

    Using form data: Manipulating form data; Resetting the form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -13,10 +13,12 @@ export class AppComponent {
   defaultQuestion = "teacher";
 
   //to show 2 way binding
-  answer = "Not answered yet."
+  defaultAnswer = "Not answered yet."
 
   genders = ["Male", "Female", "Others"];
-  selectedGender = "Male"
+  selectedGender = "Male";
+
+  submittedForm = [];
 
   // to change the value of username field
   suggestUserName() {
@@ -46,6 +48,30 @@ export class AppComponent {
    * another way is to use @viewchild
    */
   onSubmit() {
-    console.log(this.vcForm);
+    // console.log(typeof this.vcForm.value, Object.keys(this.vcForm.value), this.vcForm);
+    this.submittedForm = this.obj2ArrConvertor(this.vcForm.value);
+    this.vcForm.reset({
+      secret_question: this.defaultQuestion,
+      answer: 'Not answered yet.',
+      gender: this.selectedGender
+    });
+  }
+
+  obj2ArrConvertor(obj: Object) {
+    let retArr = [];
+    for (let [key, value] of Object.entries(obj)) {
+      // console.log(`{${key}: ${value}}`);
+      if (typeof value === 'object') {
+        // console.log('calling self', value);
+        let arr = this.obj2ArrConvertor(value);
+        retArr.push(...arr);
+      }
+      else {
+        // console.log('key:', key, 'val:', value);
+        retArr.push({'key': key, 'value': value});
+      }
+    }
+    // console.log('obj2arr', obj, retArr);
+      return retArr;
   }
 }
