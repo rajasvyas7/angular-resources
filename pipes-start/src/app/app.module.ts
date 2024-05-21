f36ee36baf0c945c12953ad0e0285942afc28360
Rajas Vyas <rajas.vyas@gmail.com>
2024-05-21 21:43:21 +0530

    Adding filter pipe at the *ngFor

--- pipes-start/src/app/app.module.ts
+++ pipes-start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EllipsisPipe } from './ellipsis.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EllipsisPipe
+    EllipsisPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
---
+++ pipes-start/src/app/filter.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filteredStatus: string, property: string): any {
+    // console.log('filter', value, filteredStatus, property);
+    
+    if (value.length === 0 || filteredStatus === '') {
+      // console.log('returning value', value);
+      return value;
+    }
+    let retArr = [];
+    for (let server of value) {
+      if (server[property] === filteredStatus) {
+        retArr.push(server);
+      }
+    }
+    // console.log('returning retArr', retArr);
+    return retArr;
+  }
+
+}
